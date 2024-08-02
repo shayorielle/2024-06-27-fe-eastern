cac358e34ae45c7fb4a5553d9b8aa59c175254b9
ediiotero <e.otero81@gmail.com>
2024-08-01 20:02:06 -0400

    week 4 done

---
+++ week-4/app.js
@@ -0,0 +1,34 @@
+var name = 'Eddie';
+var isHappy = true;
+var list = ['Tobi', 3, false];
+var obj = { key: 'value', name: 'Eddie', happy: true};
+var test = null;
+
+// console.log(name)
+// console.log(isHappy)
+// console.log(list)
+// console.log(obj)
+// console.log(test)
+
+var number = 5;
+var number2 = '5';
+var number3 = 2;
+var number4 = 2
+var text = 'Word'
+var text2 = 'Word'
+
+console.log('test 1', number == number2)
+console.log('test 2', number === number2)
+console.log('test 3', text === text2)
+console.log(number !== number2)
+console.log(number != number2)
+
+console.log(number3 > number4)
+console.log(number3 >= number4)
+
+// console.log(number + number3)
+// console.log(number - number3)
+// console.log(number / number3)
+// console.log(number * number3)
+// console.log(number % number3)
+
